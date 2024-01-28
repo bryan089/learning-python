fe40f4c531a44b11a82824f9ff5ff4392c8d9ade
Brayan <deivybrayam@gmail.com>
2024-01-28 16:41:12 -0500

    Mejoramiento en presentación de las operaciones matematicas basicas del script advance_calc2

--- advance_calc2.py
+++ advance_calc2.py
@@ -21,23 +21,38 @@ def calculator(x,y,z):
         else:
             return x / y
     elif z == '5':
-        return x + y, x - y, x * y, x / y
+        if y == 0:
+            return x + y, x - y, x * y, 'No es posible dividir entre cero (0)'
+        else:
+            return x + y, x - y, x * y, x / y
     else:
-        print("::: Fail, invalid option :::")
         return 'Fail, invalid option'
 
 
 n1 = float(input("Ingrese primer valor: "))
 n2 = float(input("Ingrese segundo valor: "))
 
-print(":::  MENÚ CALCULADORA :::")
+print(":::  MENÚ CALCULADORA :::\n")
 print("[1]. SUMAR")
 print("[2]. RESTAR")
 print("[3]. MULTIPLICAR")
 print("[4]. DIVIDIR")
 print("[5]. TODAS")
-
 opc = input("Digite una opción del menú: ")
 
-ans = calculator(n1,n2,opc)
-print("Resultado: ", ans)
+print("Formato de salida de resultados 1 \n")
+if opc == '1' or opc == '2' or opc == '3' or opc == '4':
+    ans = calculator(n1, n2, opc)
+    print("Resultado: ", ans)
+
+elif opc == '5':
+    ans = calculator(n1, n2, opc)
+    print("Suma: ", ans[0], '\n')
+    print("Resta: ", ans[1], '\n')
+    print("Multiplicacion: ", ans[2], '\n')
+    print("Division: ", ans[3], '\n')
+
+print("Formato de salida de resultados calculator 2 \n")
+#ans = calculator(n1,n2,opc, '\n')
+print("Resultado: ", calculator(n1, n2, opc))
+
